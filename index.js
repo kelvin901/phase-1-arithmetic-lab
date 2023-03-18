15034e91903436ab7cd0abecb082239d507e5d38
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-18 15:03:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+function multiply(num1, num2) {
+    return num1 * num2;
+    }
+   function mod(num3,num4){
+    return num3 % num4
+   }
+    console.log((24 % 5))
+    console.log(Math.max(0, 2, 3, 4, 20));
+    console.log(multiply(31, 2,))
+    console.log(Math.max(0, 2, 3, 4, 20));
 
-// Write your code here
